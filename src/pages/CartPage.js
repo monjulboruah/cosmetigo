f8d46060ea8dbceb717ee622aec297e0675f80a4
Monjul Boruah <monjulboruah@gmail.com>
2022-05-25 22:36:05 +0530

    final upload

---
+++ src/pages/CartPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ListOrderItem from '../components/CartPage/ListOrderItem'
+export default function CartPage() {
+  return (
+    <div>
+      <ListOrderItem />
+    </div>
+  )
+}
